9eb07c80c32876a09363147e3aba533a68031725
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-08 23:15:07 +0530

    1047. Remove All Adjacent Duplicates In String
    --> Optimal Stack Approach

---
+++ 01_Strings/00_1047. Remove All Adjacent Duplicates In String/01_Optimal_Stack_Approach.cpp
@@ -0,0 +1,39 @@
+// LeetCode (Remove All Adjacent Duplicates In String):
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/ 
+
+
+#include<iostream> 
+#include<string> 
+using namespace std; 
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+        string ans;
+        int i=0;
+
+        while(i < s.length()){
+            if(ans.empty()){
+                ans.push_back(s[i]);
+            }
+            else{
+                if(ans[ans.length()-1] == s[i]){
+                    ans.pop_back();
+                }
+                else{
+                    ans.push_back(s[i]);
+                }
+            }
+            i++;
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C.: O(n) -> While loop is traversing the whole input string named "s" 
+// S.C.: O(n) -> If all the adjacent elements are non-duplicates, then all of them will get pushed to "ans" stack 
+
+// Here, n = length of input string named "s" 
