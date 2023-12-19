d56cc73268c65dc7ecce2d313c378bbcee28b36e
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-19 23:59:26 +0530

    20. Valid Parentheses
    --> Optimal Approach

---
+++ 02_Stacks/00_Valid_Parentheses/00_Optimal_Approach.cpp
@@ -0,0 +1,48 @@
+// LeetCode (20. Valid Parentheses): 
+// https://leetcode.com/problems/valid-parentheses/ 
+
+
+#include<string>
+#include<stack>
+using namespace std; 
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        int stringLen = s.length();
+
+        if( (stringLen % 2) == 1 ) return false;
+
+        stack<char> st;
+
+        for(int i=0; i < stringLen; i++){
+            if(s[i] == '{' || s[i] == '[' || s[i] == '('){
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty()){
+                    if(st.top() == '{' && s[i] == '}'){
+                        st.pop();
+                    }
+                    else if(st.top() == '[' && s[i] == ']'){
+                        st.pop();
+                    }
+                    else if(st.top() == '(' && s[i] == ')'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return (st.empty()) ? true : false;
+
+    }
+};
