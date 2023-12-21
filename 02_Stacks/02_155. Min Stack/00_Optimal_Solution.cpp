b0e0d5eae7dc4849230c729c362673526b6988e9
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-21 23:02:49 +0530

    155. Min Stack
    --> Optimal Approach

---
+++ 02_Stacks/02_155. Min Stack/00_Optimal_Solution.cpp
@@ -0,0 +1,61 @@
+// LeetCode (155. Min Stack): 
+// https://leetcode.com/problems/min-stack/ 
+
+
+#include<vector>
+#include<utility>
+using namespace std;
+
+
+class MinStack {
+
+private:
+    vector<pair<int, int>> v;
+
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(v.empty()){
+            pair<int, int> p;
+            p.first = val;
+            p.second = val;
+            v.push_back(p);
+        }
+        else{
+            pair<int, int> p;
+            p.first = val;
+            p.second = min(val, v.back().second);
+            v.push_back(p);
+        }
+    }
+    
+    void pop() {
+        v.pop_back();
+    }
+    
+    int top() { 
+        return v.back().first;
+    }
+    
+    int getMin() {
+        return v.back().second;
+    }
+};
+
+
+/*
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+
+// T.C.: O(1) 
+// S.C.: O(1) 
+
