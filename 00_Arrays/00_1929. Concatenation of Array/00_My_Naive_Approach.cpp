8ab0a1ee1ca7bd99d9e54938f9a0678f266aa63d
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-23 23:23:03 +0530

    1929. Concatenation of Array
    --> My Naive Approach

---
+++ 00_Arrays/00_1929. Concatenation of Array/00_My_Naive_Approach.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans(2*n);        // t.c = O(2n), s.c. = O(2n) 
+
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+
+        // for loop has t.c. O(n) 
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(2n) + O(n) = O(3n) = O(n) 
+// S.C. = O(n) 
+
+// Here, n = number of elements present in input vector named "nums" 
