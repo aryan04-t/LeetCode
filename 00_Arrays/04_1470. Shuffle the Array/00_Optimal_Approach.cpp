ebd71a893565de3048adfe8e66ece1f7cd3cef3d
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-25 04:50:16 +0530

    1470. Shuffle the Array
    --> Optimal Approach

---
+++ 00_Arrays/04_1470. Shuffle the Array/00_Optimal_Approach.cpp
@@ -0,0 +1,28 @@
+// LeetCode (Shuffle the Array): 
+// https://leetcode.com/problems/shuffle-the-array/ 
+
+
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        
+        vector<int> ans;
+
+        for(int i=0, j=n; i < n; i++, j++){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n/2) = O(n) 
+// S.C. = O(n) 
+
+// Here, n = number of elements which are present inside the input vector named "nums" 
