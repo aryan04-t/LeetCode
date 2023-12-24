819612f3569d0973bf63a6eb83e3dff149c7d1c1
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-25 04:54:07 +0530

    2942. Find Words Containing Character
    --> Optimal Approach

---
+++ 00_Arrays/05_2942. Find Words Containing Character/00_Optimal_Approach.cpp
@@ -0,0 +1,34 @@
+// LeetCode (Find Words Containing Character): 
+// https://leetcode.com/problems/find-words-containing-character/ 
+
+
+#include<vector>
+#include<string>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        
+        int n = words.size();
+        vector<int> ans;
+
+        for(int i=0; i < n; i++){
+            for(int j=0; j < words[i].length(); j++){
+                if(words[i][j] == x){
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+// T.C. = O(n*m) 
+// S.C. = O(n) 
+
+// Here, n = number of elements which are present in input vector named "words", and m = average length of strings inside the input vector named "words" 
