d8b02e77a8810b072ea1a1d41a67dda9061ee085
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-26 23:37:45 +0530

    2798. Number of Employees Who Met the Target
    --> Optimal Approach

---
+++ 01_Arrays/007_2798. Number of Employees Who Met the Target/00_Optimal_Approach.cpp
@@ -0,0 +1,30 @@
+// LeetCode (Number of Employees Who Met the Target): 
+// https://leetcode.com/problems/number-of-employees-who-met-the-target/ 
+
+
+#include<vector> 
+using namespace std; 
+
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        
+        int n = hours.size();
+        int count = 0;
+
+        for(int i=0; i < n; i++){
+            if(hours[i] >= target){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// Here, n = number of elements present inside the input vector named "hours" 
