8e6b71a39a9358e3d3d33e5c0f2f6e8fefaf882f
Aryan Tomar <aryantomar5000@gmail.com>
2024-01-02 22:23:53 +0530

    35. Search Insert Position
    --> Optimal Approach

---
+++ 05_Binary Search/000_35. Search Insert Position/00_Optimal_Approach.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        
+        int start = 0;
+        int end = n-1;
+        int mid;
+
+        while(start <= end){
+
+            mid = start + (end - start)/2;    
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                start = mid + 1;
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+};
+
+
+// T.C. = O(log(n) to the base 2) 
+// S.C. = O(1) 
+
+// Here, n = number of elements present inside the input vector named "nums" 
