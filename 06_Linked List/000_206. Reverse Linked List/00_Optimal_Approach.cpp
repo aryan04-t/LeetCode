5feb2a981e590b065e23ffabfe9dedeb3d77cbc8
Aryan Tomar <aryantomar5000@gmail.com>
2024-01-04 23:59:16 +0530

    206. Reverse Linked List
    --> Optimal Approach

---
+++ 06_Linked List/000_206. Reverse Linked List/00_Optimal_Approach.cpp
@@ -0,0 +1,46 @@
+class ListNode{
+    public:
+        int val;
+        ListNode* next;
+};
+
+
+// - - - - - - - - - - 
+
+
+/*
+    Definition for singly-linked list.
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+*/
+
+class Solution {
+public:
+    
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* prevNode = nullptr;
+        ListNode* currNode = head;
+        ListNode* nextNode;
+
+        while(currNode != nullptr){
+            nextNode = currNode->next;
+            currNode->next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        return prevNode;
+    }
+
+};
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// Here, n = total number of nodes present in the singly-linked list 
