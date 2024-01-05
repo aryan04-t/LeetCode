7b8a492ea6d408424ca5a48b74d8209c3fc3216a
Aryan Tomar <aryantomar5000@gmail.com>
2024-01-05 23:55:10 +0530

    387. First Unique Character in a String
    --> An Approach Using Vector and Queue

--- 02_Strings/005_387. First Unique Character in a String/01_An_Approach_Using_Vector_and_Queue.cpp
+++ 02_Strings/005_387. First Unique Character in a String/01_An_Approach_Using_Vector_and_Queue.cpp
@@ -1,5 +1,6 @@
 #include<string>
 #include<vector> 
+#include<queue>
 using namespace std; 
 
 
@@ -10,23 +11,30 @@ public:
         int n = s.length();
 
         vector<int> charFreqCount(26, 0);
-        
-        for(char ch : s){
-            charFreqCount[ch - 'a']++; 
+        queue<int> q;
+
+        for(int i=0; i < n; i++){
+            charFreqCount[s[i] - 'a']++;
+            if(charFreqCount[s[i] - 'a'] == 1){
+                q.push(i);
+            }
         }
 
-        for(int i = 0; i < n; i++){
-            if(charFreqCount[s[i] - 'a'] == 1){
-                return i;
+        while(!q.empty()){
+            if(charFreqCount[s[q.front()] - 'a'] > 1){
+                q.pop();
+            }
+            else{
+                return q.front();
             }
         }
 
         return -1; 
     }
-}; 
+};
 
 
 // T.C. = O(n) + O(n) = O(2n) = O(n) 
-// S.C. = O(1) 
+// S.C. = O(1) + O(n) = O(n) 
 
 // Here, n = length of input string named "s" 
