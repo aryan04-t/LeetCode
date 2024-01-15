8cbb34bd7c5dcfb6f55e608b11780b80ecd8392e
Aryan Tomar <aryantomar5000@gmail.com>
2024-01-15 22:31:24 +0530

    24. Swap Nodes in Pairs
    --> Optimal Clean Recursive Code

---
+++ 06_Linked List/004_24. Swap Nodes in Pairs/02_Optimal_Clean_Recursive_Code.cpp
@@ -0,0 +1,49 @@
+// LeetCode (24. Swap Nodes in Pairs): 
+// https://leetcode.com/problems/swap-nodes-in-pairs/ 
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+// - - - - - - - - - - 
+
+
+/*
+    Definition for singly-linked list.
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+*/
+
+
+class Solution {
+public:
+
+    ListNode* swapPairs(ListNode* head) {
+        
+        if(head == nullptr || head->next == nullptr) return head;
+
+        ListNode* nextNode = head->next;
+        head->next = swapPairs(head->next->next);
+        nextNode->next = head;
+
+        return nextNode;
+    
+    }
+};
+
+
+// T.C. = O(n/2) = O(n) 
+// S.C. = O(n/2) = O(n) -> (This Space Complexity is caused due to Function Call Stack) 
+
+// Here, n = the total number of nodes which are present inside the singly linked list 
