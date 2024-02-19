1f07fde68d9d76dfc25cf0bac9ad2c33f6cf6a1e
Aryan Tomar <aryantomar5000@gmail.com>
2024-02-19 20:28:38 +0530

    28. Find the Index of the First Occurrence in a String
    --> Optimal Approach using STL

---
+++ 02_Strings/016_28. Find the Index of the First Occurrence in a String/01_Optimal_Approach_using_STL.cpp
@@ -0,0 +1,34 @@
+// LeetCode (28. Find the Index of the First Occurrence in a String): 
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/ 
+
+
+#include<string> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    int strStr(string haystack, string needle) {
+        
+        int n = haystack.length();
+        int m = needle.length();
+
+        for(int i=0; i < n - m + 1; i++){
+            
+            if(needle == haystack.substr(i, m)){
+                return i;
+            }
+
+        }
+
+        return -1;
+
+    }
+};
+
+
+// T.C. = O(n-m+1) * O(m) = O(n*m) - O(m^2) + O(m) = O(n*m) 
+// S.C. = O(1) 
+
+// Here, n = the length of input string named "haystack", and m = the length of input string named "needle" 
