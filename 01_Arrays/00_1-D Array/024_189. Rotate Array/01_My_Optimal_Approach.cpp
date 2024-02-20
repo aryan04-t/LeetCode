0890be76a4a5243f525a4ae143d9e32cd24f6e76
Aryan Tomar <aryantomar5000@gmail.com>
2024-02-20 11:18:20 +0530

    189. Rotate Array
    --> My Optimal Approach

---
+++ 01_Arrays/00_1-D Array/024_189. Rotate Array/01_My_Optimal_Approach.cpp
@@ -0,0 +1,40 @@
+// LeetCode (189. Rotate Array): 
+// https://leetcode.com/problems/rotate-array/ 
+
+
+#include<vector> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+
+        k = k % n;
+
+        vector<int> rotatedNums;
+
+        for(int i=n-k; i < n; i++){
+            rotatedNums.push_back(nums[i]); 
+        }
+
+        for(int i=0; i < n-k; i++){
+            rotatedNums.push_back(nums[i]); 
+        }
+
+        for(int i=0; i < n; i++){
+            nums[i] = rotatedNums[i];
+        }
+
+    }
+
+};
+
+
+// T.C. = O(n) + O(n) = O(2n) = O(n) 
+// S.C. = O(n) 
+
+// Here, n = total number of elements which are present inside the input vector named "nums", and k = the value of input variable named "k" 
