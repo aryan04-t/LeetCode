144005358146e144021cb61f8d89ec867ddf76f8
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-03 16:16:33 +0530

    345. Reverse Vowels of a String
    --> Most Optimal 2-Pointer Approach Code

---
+++ 02_Strings/012_345. Reverse Vowels of a String/03_Most_Optimal_2_Pointer_Approach_Code.cpp
@@ -0,0 +1,67 @@
+// LeetCode (345. Reverse Vowels of a String): 
+// https://leetcode.com/problems/reverse-vowels-of-a-string/ 
+
+
+#include<vector> 
+#include<string> 
+#include<utility> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    bool isVowel(const char &ch){
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            return true; 
+        } 
+
+        if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            return true; 
+        }
+
+        return false; 
+
+    }
+
+    string reverseVowels(string s) {
+
+        int n = s.length(); 
+
+        int i = 0; 
+        int j = n-1; 
+
+        while(i < j){
+            
+            while( i < j && !isVowel(s[i]) ){
+                i++;
+            }
+
+            while( i < j && !isVowel(s[j]) ){
+                j--;
+            }
+
+            if( i < j && isVowel(s[i]) && isVowel(s[j]) ){
+                
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+
+                i++;
+                j--;
+
+            }
+
+        }
+
+        return s;
+
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// Here, n = the total length of input string named "s" 
