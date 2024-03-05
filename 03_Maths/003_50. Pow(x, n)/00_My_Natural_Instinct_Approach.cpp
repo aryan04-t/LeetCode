a90ca3c74e6c975132dde93b85612937d5163e0c
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-05 23:15:52 +0530

    50. Pow(x, n)
    --> My Natural Instinct Approach ;)

---
+++ 03_Maths/003_50. Pow(x, n)/00_My_Natural_Instinct_Approach.cpp
@@ -0,0 +1,18 @@
+// LeetCode (50. Pow(x, n)): 
+// https://leetcode.com/problems/powx-n/ 
+
+
+#include<cmath>
+using namespace std;
+
+
+class Solution {
+public:
+
+    double myPow(double x, int n) {
+
+        return pow(x, n); 
+
+    }
+
+};
