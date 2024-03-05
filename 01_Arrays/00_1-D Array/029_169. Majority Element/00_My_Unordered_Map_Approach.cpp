8bb28dbb104d5e248ccda3d7a04415d0baa2936b
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-05 21:08:17 +0530

    169. Majority Element
    --> My Unordered Map Approach

---
+++ 01_Arrays/00_1-D Array/029_169. Majority Element/00_My_Unordered_Map_Approach.cpp
@@ -0,0 +1,43 @@
+// LeetCode (169. Majority Element): 
+// https://leetcode.com/problems/majority-element/ 
+
+
+#include<vector> 
+#include<unordered_map> 
+#include<utility> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    int majorityElement(vector<int>& nums) {
+
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int i=0; i < n; i++){
+            mp[nums[i]]++;
+        }
+
+        int maxi = INT_MIN; 
+        int ans = nums[0];
+
+        for(const pair<int, int> &p : mp){
+            if(p.second > maxi){
+                maxi = p.second;
+                ans = p.first;
+            }
+        }
+
+        return ans;
+
+    }
+
+};
+
+
+// T.C. = O(n) + O(n) = O(2n) = O(n) 
+// S.C. = O(n/2) = O(n) 
+
+// Here, n = total number of elements which are present inside the input vector named "nums" 
