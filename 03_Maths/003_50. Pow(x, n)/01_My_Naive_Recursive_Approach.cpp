25cd5501b3f638082d4cee73db12166b260ea1d7
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-06 13:00:42 +0530

    50. Pow(x, n) ddnfksdkjndfkvnfdkjvndfkjvndfkjnvfdlfmdsl
    --> Handled "n = INT_MIN" edge case in "My Naive
    Recursive Approach Code", and I have added Time
    and Space Complexity of my approach to the code

--- 03_Maths/003_50. Pow(x, n)/01_My_Naive_Recursive_Approach.cpp
+++ 03_Maths/003_50. Pow(x, n)/01_My_Naive_Recursive_Approach.cpp
@@ -9,26 +9,44 @@ using namespace std;
 class Solution {
 public:
 
-    double getPositivePow(double x, int n){
+    double getPositivePower(double x, int n){
 
-        if(n == 0) return 1.0; 
+        if(n == 0){
+            return 1.0; 
+        }
 
-        return x * myPow(x, n-1); 
+        return x * getPositivePower(x, n-1); 
+
+    }
+
+    double getNegativePower(double x, int n){
+
+        if(n == 0){
+            return 1.0; 
+        }
+
+        return x * getNegativePower(x, n+1); 
 
     }
 
     double myPow(double x, int n) {
 
-        if(n >= 0){
-            return getPositivePow(x, n);
+        if(x == 1.0) return x; 
+        if(n == 0) return 1.0; 
+
+        if(n > 0){
+            return getPositivePower(x, n); 
         }
         else{
-            n = abs(n);
-            double ans = 1.0;
-            double power = getPositivePow(x, n);
-            return ans / power;
+            return 1.0 / getNegativePower(x, n); 
         }
-        
+
     }
 
 };
+
+
+// T.C. = O(n) 
+// S.C. = O(n) -> This space complexity is caused due to function call stack 
+
+// Here, n = the value of input variable named "n" 
