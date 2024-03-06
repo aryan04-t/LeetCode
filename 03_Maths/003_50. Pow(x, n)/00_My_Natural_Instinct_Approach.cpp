f54abde43d6011c385a985639844c6e185c67b0e
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-06 13:17:39 +0530

    50. Pow(x, n)
    --> Added Time and Space complexity to
    "My Natural Instinct Approach"

--- 03_Maths/003_50. Pow(x, n)/00_My_Natural_Instinct_Approach.cpp
+++ 03_Maths/003_50. Pow(x, n)/00_My_Natural_Instinct_Approach.cpp
@@ -11,8 +11,14 @@ public:
 
     double myPow(double x, int n) {
 
-        return pow(x, n); 
+        return pow(x, n);
 
     }
 
 };
+
+
+// T.C. = O(log(n) to the base 2) 
+// S.C. = O(1) 
+
+// Here, n = the value of input variable named "n" 
