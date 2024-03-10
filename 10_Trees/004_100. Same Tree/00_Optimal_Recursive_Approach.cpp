8b6a11391efc8a8680c88922f44af3c4d871a563
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-10 13:43:58 +0530

    100. Same Tree
    --> Updated "Optimal Recursive Approach" Space
    Complexity

--- 10_Trees/004_100. Same Tree/00_Optimal_Recursive_Approach.cpp
+++ 10_Trees/004_100. Same Tree/00_Optimal_Recursive_Approach.cpp
@@ -32,25 +32,24 @@ class Solution {
 public:
 
     bool isSameTree(TreeNode* p, TreeNode* q) {
-
+        
         if(p == nullptr && q == nullptr) return true; 
-        else if(p != nullptr && q == nullptr) return false;
-        else if(p == nullptr && q != nullptr) return false;
+        else if(p != nullptr && q == nullptr) return false; 
+        else if(p == nullptr && q != nullptr) return false; 
 
         if(p->val != q->val) return false; 
 
-        bool leftCheck = isSameTree(p->left, q->left); 
-        bool rightCheck = isSameTree(p->right, q->right); 
+        bool checkLeft = isSameTree(p->left, q->left); 
+        bool checkRight = isSameTree(p->right, q->right);  
 
-        return (leftCheck && rightCheck);
-    
+        return (checkLeft && checkRight); 
+
     }
-
-};
+}; 
 
 
 // T.C. = O(min(n, m)) 
-// S.C. = O( min( min(n, m), h) ) -> This space complexity is caused due to function call stack 
+// S.C. = O(min(h1, h2)) -> This spacce complexity is caused due to function call stack 
 
-// Here, n = the total number of nodes which are present inside the input tree-1, and m = the total number of node which are present inside the input tree-2 
-// and h = the height of the smaller tree 
+// Here, n = total number of nodes which are present inside the input binary tree named "p", and m = total number of nodes which are present inside the input binary tree named "q" 
+// h1 = the height of the input binary tree named "p", and h2 = the height of the input binary tree named "q" 
