75b773fa3947209a8887e61fff968b8aed3bd4dd
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-19 21:44:46 +0530

    113. Path Sum II
    --> My Naive DFS Approach

---
+++ 10_Trees/013_113. Path Sum II/00_My_Naive_DFS_Approach.cpp
@@ -0,0 +1,78 @@
+// LeetCode (113. Path Sum II): 
+// https://leetcode.com/problems/path-sum-ii/ 
+
+
+#include<vector> 
+using namespace std; 
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+// - - - - - - - - - - 
+
+
+/*
+    Definition for a binary tree node.
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+*/
+
+
+class Solution {
+public:
+
+    void findAllPaths(TreeNode* root, int targetSum, vector<int> &currPath, vector<vector<int>> &ans){
+
+        if(root == nullptr) return; 
+        
+        currPath.push_back(root->val);
+
+        if(root->left == nullptr && root->right == nullptr){
+
+            int sum = 0;
+            for(int val : currPath) sum += val; 
+
+            if(sum == targetSum) ans.push_back(currPath); 
+            currPath.pop_back(); 
+
+            return; 
+        }
+
+        findAllPaths(root->left, targetSum, currPath, ans); 
+        findAllPaths(root->right, targetSum, currPath, ans); 
+
+        currPath.pop_back(); 
+
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+
+        vector<int> currPath; 
+        vector<vector<int>> ans; 
+
+        findAllPaths(root, targetSum, currPath, ans); 
+
+        return ans; 
+
+    }
+};
+
+
+// T.C. = O(n) + O(m*h) = O(m*h) 
+// S.C. = O(h) 
+
+// Here, n = total number of nodes which are present in the given binary tree, m = total number of leaf nodes which are present in the given binary tree, and h = the height of the given binary tree 
