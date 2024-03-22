0cb725ad37aaf0c872883c9d724efc2d6b7307d6
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-22 20:27:21 +0530

    1302. Deepest Leaves Sum
    --> Another Easier Optimal DFS Approach

---
+++ 10_Trees/006_1302. Deepest Leaves Sum/01_Another_Easier_Optimal_DFS_Approach.cpp
@@ -0,0 +1,76 @@
+// LeetCode (1302. Deepest Leaves Sum): 
+// https://leetcode.com/problems/deepest-leaves-sum/ 
+
+
+#include<algorithm> 
+using namespace std; 
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+// - - - - - - - - - - 
+
+
+/*
+    Definition for a binary tree node.
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+*/
+
+
+class Solution {
+public:
+
+    int getDepth(TreeNode* root){
+
+        if(root == nullptr) return 0; 
+
+        int leftDepth = getDepth(root->left);
+        int rightDepth = getDepth(root->right);
+
+        return max(leftDepth, rightDepth) + 1; 
+
+    }
+
+    void findSum(TreeNode* root, int depth, int &sum){
+
+        if(root == nullptr) return; 
+
+        if(depth == 1) sum += root->val; 
+
+        findSum(root->left, depth-1, sum);
+        findSum(root->right, depth-1, sum); 
+
+    }
+
+    int deepestLeavesSum(TreeNode* root) {
+
+        int depth = getDepth(root); 
+
+        int sum = 0;
+        findSum(root, depth, sum); 
+
+        return sum; 
+
+    }
+};
+
+
+// T.C. = O(n) + O(n) = O(n) 
+// S.C. = O(h) 
+
+// Here, n = the total number of nodes which are present inside the given binary tree, and h = the height of the given binary tree 
