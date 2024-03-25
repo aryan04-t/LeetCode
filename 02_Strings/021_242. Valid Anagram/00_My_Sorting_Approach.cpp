cf4f379610ee1d84f30d5b526fd52ca15193e845
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-26 01:39:04 +0530

    242. Valid Anagram
    --> My Sorting Approach

---
+++ 02_Strings/021_242. Valid Anagram/00_My_Sorting_Approach.cpp
@@ -0,0 +1,36 @@
+// LeetCode (242. Valid Anagram): 
+// https://leetcode.com/problems/valid-anagram/ 
+
+
+#include<string> 
+#include<algorithm> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    bool isAnagram(string s, string t) {
+
+        int n = s.size(); 
+        int m = t.size(); 
+
+        if(n != m) return false; 
+
+        sort(s.begin(), s.end()); 
+        sort(t.begin(), t.end()); 
+
+        for(int i=0; i < n; i++){
+            if(s[i] != t[i]) return false; 
+        }
+
+        return true; 
+
+    }
+};
+
+
+// T.C. = O(n * log(n)) 
+// S.C. = O(log(n)) 
+
+// Here, n = the length of the input string named "s" / "t" = m 
