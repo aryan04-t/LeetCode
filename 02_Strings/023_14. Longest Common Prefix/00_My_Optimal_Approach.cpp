b16659e4b274d373809dbbf47cd8102f75b83970
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-26 03:42:11 +0530

    14. Longest Common Prefix
    --> My Optimal Approach

---
+++ 02_Strings/023_14. Longest Common Prefix/00_My_Optimal_Approach.cpp
@@ -0,0 +1,55 @@
+// LeetCode (14. Longest Common Prefix): 
+// https://leetcode.com/problems/longest-common-prefix/ 
+
+
+#include<vector> 
+#include<string> 
+#include<limits.h> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        int n = strs.size(); 
+        int minLen = INT_MAX; 
+
+        for(int i=0; i < n; i++){
+            int len = strs[i].size(); 
+            minLen = min(len, minLen); 
+        }
+
+        string ans = ""; 
+
+        for(int i=0; i < minLen; i++){
+
+            char c = strs[0][i]; 
+            bool isPartOfPrefix = true;
+            
+            for(int j=1; j < n; j++){
+                if(strs[j][i] != c){
+                    isPartOfPrefix = false; 
+                    break;
+                }
+            }
+
+            if(isPartOfPrefix){
+                ans.push_back(c);
+            }
+            else{
+                break; 
+            }
+        }
+
+        return ans;
+
+    }
+};
+
+
+// T.C = O(n) + O(m * n) = O(m * n) 
+// S.C. = O(m) 
+
+// Here, n = the total number of strings which are present inside the input vector named "strs", and m = the length of the smallest string in the input vector "strs" 
