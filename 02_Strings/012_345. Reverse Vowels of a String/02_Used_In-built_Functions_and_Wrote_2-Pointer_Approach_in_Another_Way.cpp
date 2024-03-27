98d9993bb6fed4b73e85f6c15cfce4316910733e
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-27 23:52:35 +0530

    345. Reverse Vowels of a String
    --> Used in-built functions and wrote "2-Pointer
    Approach" in another way

--- 02_Strings/012_345. Reverse Vowels of a String/02_Used_In-built_Functions_and_Wrote_2-Pointer_Approach_in_Another_Way.cpp
+++ 02_Strings/012_345. Reverse Vowels of a String/02_Used_In-built_Functions_and_Wrote_2-Pointer_Approach_in_Another_Way.cpp
@@ -3,19 +3,19 @@
 
 
 #include<string> 
+#include<cctype> 
+#include<utility> 
 using namespace std; 
 
 
 class Solution {
 public:
 
-    bool isVowel(const char &ch){
+    bool isVowel(char ch){
+
+        ch = tolower(ch);
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-            return true; 
-        } 
-
-        if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
             return true; 
         }
 
@@ -31,15 +31,18 @@ public:
         int j = n-1; 
 
         while(i < j){
-            while( i < j && !isVowel(s[i]) ) i++;
-            while( i < j && !isVowel(s[j]) ) j--;
-
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-
-            i++;
-            j--;
+            if( isVowel(s[i]) && isVowel(s[j]) ){
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+            else if( !isVowel(s[i]) ){
+                i++;
+            }
+            // else if( !isVowel(s[j]) ){
+            else{
+                j--;
+            }
         }
 
         return s;
