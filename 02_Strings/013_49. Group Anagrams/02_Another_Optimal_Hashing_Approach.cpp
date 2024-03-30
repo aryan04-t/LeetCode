2765870c38922abd2027a7637e1fe031c31fb511
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-30 15:35:44 +0530

    49. Group Anagrams
    --> Another Optimal Hashing Approach

---
+++ 02_Strings/013_49. Group Anagrams/02_Another_Optimal_Hashing_Approach.cpp
@@ -0,0 +1,64 @@
+// LeetCode (49. Group Anagrams): 
+// https://leetcode.com/problems/group-anagrams/ 
+
+
+#include<vector> 
+#include<string> 
+#include<unordered_map> 
+using namespace std;
+
+
+class Solution {
+public:
+
+    string getSignature(const string &s){
+
+        int m = s.length(); 
+        vector<int> charFreq(26, 0); 
+
+        for(int i=0; i < m; i++){
+            charFreq[s[i] - 'a']++;
+        }
+
+        string sign; 
+
+        for(int i=0; i < 26; i++){
+            if(charFreq[i] != 0){
+                sign.push_back( char(i + 'a') ); 
+                sign += to_string(charFreq[i]);
+            }
+        }
+
+        return sign; 
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, int> mp; 
+        vector<vector<string>> ans; 
+
+        int n = strs.size(); 
+
+        for(int i=0; i < n; i++){
+            
+            string sign = getSignature(strs[i]); 
+
+            if(mp.find(sign) == mp.end()){
+                mp[sign] = ans.size(); 
+                ans.push_back({strs[i]}); 
+            }
+            else{
+                int index = mp[sign]; 
+                ans[index].push_back(strs[i]); 
+            }
+        }
+
+        return ans; 
+    }
+}; 
+
+
+// T.C. = O(n*k) 
+// S.C. = O(26) + O(l*k) + O(n*k) = O(n*k) 
+
+// Here, n = the total number of strings which are present inside the input vector named "strs", and k = the average length of all the strings which are present inside the input vector named "strs", and l = the total number of unique anagram signatures which are created 
