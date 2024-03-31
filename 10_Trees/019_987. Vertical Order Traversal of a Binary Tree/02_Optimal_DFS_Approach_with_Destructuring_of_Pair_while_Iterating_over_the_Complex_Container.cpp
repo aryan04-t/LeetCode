c191a41bf01c9b24d6adf8aab6eebea87cd4188b
Aryan Tomar <aryantomar5000@gmail.com>
2024-03-31 22:08:29 +0530

    987. Vertical Order Traversal of a Binary Tree
    --> Optimal DFS Approach with Destructuring of
    Pair while Iterating over the Complex Container

---
+++ 10_Trees/019_987. Vertical Order Traversal of a Binary Tree/02_Optimal_DFS_Approach_with_Destructuring_of_Pair_while_Iterating_over_the_Complex_Container.cpp
@@ -0,0 +1,82 @@
+// LeetCode (987. Vertical Order Traversal of a Binary Tree): 
+// https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/ 
+
+
+#include<map> 
+#include<set> 
+#include<utility>
+#include<vector> 
+using namespace std; 
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+// - - - - - - - - - - 
+
+
+/*
+    Definition for a binary tree node.
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+*/
+
+
+class Solution {
+public: 
+
+    void findVerticalTraversal(TreeNode* root, int level, int horizontalDist, map<int, multiset<pair<int, int>>> &container){
+
+        if(root == nullptr) return; 
+
+        container[horizontalDist].insert({level, root->val}); 
+
+        findVerticalTraversal(root->left, level + 1, horizontalDist - 1, container);
+        findVerticalTraversal(root->right, level + 1, horizontalDist + 1, container);
+
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+
+        int level = 0; 
+        int horizontalDist = 0; 
+        
+        map<int, multiset<pair<int, int>>> container; 
+        findVerticalTraversal(root, level, horizontalDist, container); 
+
+        vector<vector<int>> ans; 
+        
+        for(const auto &[horizontalDist, mset] : container){
+            
+            vector<int> temp;
+
+            for(const auto &[level, val] : mset){
+                temp.push_back(val); 
+            }
+
+            ans.push_back(temp); 
+        }
+
+        return ans; 
+    }
+};
+
+
+// T.C. = O(n) + O( n * (log(m) to the base 2) * (log(k) to the base 2)) = O(n * log(m) * log(k)) 
+// S.C. = O(h) + O(n) + O(n) = O(n) 
+
+// Here, n = the total number of nodes which are present inside the given binary tree, and h = the height of the given binary tree 
+// m = the total number of unique horizontal distances which are present in the given binary tree, and k = the maximum number of nodes which are present at any given horizontal distance in the given binary tree 
