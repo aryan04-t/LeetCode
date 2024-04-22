e527ca2766834c9354912a59d6d47c13163a1db9
Aryan Tomar <aryantomar5000@gmail.com>
2024-04-22 11:21:47 +0530

    29. Divide Two Integers
    --> Explanation of why I have put this question into
    "Binary Search Category", although this isn't a binary
    search question

---
+++ 05_Binary Search/008_29. Divide Two Integers/03_Why_I_Have_Put_this_Question_into_Binary_Search_Category.cpp
@@ -0,0 +1,15 @@
+/*
+
+
+# Why I have put this Question into "Binary Search Category": 
+
+1. This question is not a "Binary Search Question" 
+2. This is a "Bit Manipulation Question", and every other approach that I have used for solving this question until now, all those approaches are invalid approaches. 
+3. The usage of divison operator is not allowed for solving this question, because the question explicitly mentions not to use the division operator, that's why my first "Division Operator Approach" is invalid. 
+4. The subtraction approach is a valid approach, but you get "Time Limit Exceeded Error" when you use it, because it is a very naive approach. 
+5. The binary search approach passes all the test cases, and it is a good approach, but again, the binary search approach also uses multiplication operator, and the question mentions not to use the multiplication operator also, so this one is also a invalid approach  
+
+--> But still, before learning the "Bit Manipulation Approach", I wanted to try all these approaches once, as all of these appraoches clicked in my mind on my own, and the binary search approach is also an "Intutive One", and that's also the reason I have put this question into the "Binary Search Folder"
+
+
+*/ 
