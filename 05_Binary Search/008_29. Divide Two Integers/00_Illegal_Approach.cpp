cbc2ff9f13f339dc75d7908b09edb66e908e2c4f
Aryan Tomar <aryantomar5000@gmail.com>
2024-04-22 10:41:55 +0530

    29. Divide Two Integers
    --> Illegal Approach

---
+++ 05_Binary Search/008_29. Divide Two Integers/00_Illegal_Approach.cpp
@@ -0,0 +1,30 @@
+// 29. Divide Two Integers (29. Divide Two Integers): 
+// https://leetcode.com/problems/divide-two-integers/ 
+
+
+#include<iostream> 
+#include<limits.h> 
+using namespace std; 
+
+
+class Solution {
+public: 
+
+    int divide(int dividend, int divisor) {
+
+        long long Dividend = dividend; 
+        long long Divisor = divisor; 
+
+        long long ans = Dividend/Divisor; 
+
+        if(ans > INT_MAX) ans = INT_MAX;
+        else if(ans < INT_MIN) ans = INT_MIN; 
+
+        int finalAns = int(ans); 
+
+        return finalAns; 
+    }
+};
+
+
+// Illegal Approach for this Question 
