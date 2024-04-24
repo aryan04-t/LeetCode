ed82ec409f7a1f4cc5ec02c7ddf3020941d3b380
Aryan Tomar <aryantomar5000@gmail.com>
2024-04-24 07:02:49 +0530

    912. Sort an Array
    --> Unoptimized Bubble Sort Code with Its Time and
    Space Complexity

---
+++ 09_Sorting/000_912. Sort an Array/01_Bubble Sort/00_Unoptimized_Bubble_Sort_with_Its_Time_and_Space_Complexity.cpp
@@ -0,0 +1,46 @@
+// LeetCode (912. Sort an Array): 
+// https://leetcode.com/problems/sort-an-array/ 
+
+
+// This code gives TLE (Time Limit Exceeded) Error 
+
+
+#include<vector> 
+using namespace std; 
+
+
+// Unoptimized Bubble Sort 
+
+class Solution {
+public:
+
+    vector<int> sortArray(vector<int>& nums) {
+
+        int n = nums.size();
+
+        for(int i=0; i < n-1; i++){
+            for(int j=0; j < n-i-1; j++){
+                if(nums[j] > nums[j+1]){
+                    swap(nums[j], nums[j+1]); 
+                }
+            }
+        }
+
+        return nums; 
+    }
+};
+
+
+/* 
+
+# T.C. = O((n * (n-1))/2) = O(n^2) 
+     Best Case    |    Average Case   |    Worst Case   | 
+      O(n^2)      |       O(n^2)      |      O(n^2)     | 
+
+# S.C. = O(1) 
+     Best Case    |    Average Case   |    Worst Case   | 
+       O(1)       |       O(1)        |      O(1)       | 
+
+Here, n = the total number of elements which are present inside the input vector named "nums" 
+
+*/ 
