ec2dd247f410e48d012a4b0485aec9a26d2d5aa7
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-03 15:44:50 +0530

    165. Compare Version Numbers
    --> My Most Optimal Two Pointer Approach

---
+++ 02_Strings/031_165. Compare Version Numbers/00_My_Most_Optimal_Two_Pointer_Approach.cpp
@@ -0,0 +1,53 @@
+// LeetCode (165. Compare Version Numbers): 
+// https://leetcode.com/problems/compare-version-numbers/ 
+
+
+#include<string> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    int compareVersion(string version1, string version2) {
+        
+        int ans = 0; 
+        int n = version1.length(), m = version2.length();
+        int i=0, j=0;
+
+        while(i < n || j < m){
+
+            int num1 = 0; 
+            while(i < n && version1[i] != '.'){
+                num1 = num1 * 10 + int(version1[i] - '0'); 
+                i++;
+            }
+
+            int num2 = 0; 
+            while(j < m && version2[j] != '.'){
+                num2 = num2 * 10 + int(version2[j] - '0'); 
+                j++; 
+            }
+
+            if(num1 < num2 && ans == 0){
+                ans = -1; 
+                break; 
+            }
+            else if(num1 > num2 && ans == 0){
+                ans = 1; 
+                break; 
+            }
+
+            i++; 
+            j++; 
+        }
+
+        return ans; 
+    }
+};
+
+
+// T.C. = O(max(n, m)) 
+// S.C. = O(1) 
+
+// Here, n = the length of the input string named of "version1", and m = the length of the input string named "version2" 
