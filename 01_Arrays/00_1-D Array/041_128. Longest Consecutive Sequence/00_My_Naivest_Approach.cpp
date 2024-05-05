3fd27414e585fe2a805758480ed246ec7124832d
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-05 07:03:46 +0530

    128. Longest Consecutive Sequence
    --> My Naivest Approach

---
+++ 01_Arrays/00_1-D Array/041_128. Longest Consecutive Sequence/00_My_Naivest_Approach.cpp
@@ -0,0 +1,41 @@
+// LeetCode (128. Longest Consecutive Sequence): 
+// https://leetcode.com/problems/longest-consecutive-sequence/ 
+
+
+#include<vector> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    bool isPresent(vector<int> &nums, int a){
+        for(int val : nums){
+            if(a == val) return true;
+        }
+        return false;
+    }
+
+    int longestConsecutive(vector<int>& nums) {
+
+        int n = nums.size(); 
+        int finalAns = 0; 
+
+        for(int val : nums){
+            int ans = 1; 
+            while(isPresent(nums, val+1)){
+                val++; 
+                ans++; 
+            }
+            if(ans > finalAns) finalAns = ans;  
+        }
+
+        return finalAns; 
+    }
+};
+
+
+// T.C. = O(n^3) 
+// S.C. = O(1) 
+
+// Here, n = the total number of elements which are present inside the input vector named "nums" 
