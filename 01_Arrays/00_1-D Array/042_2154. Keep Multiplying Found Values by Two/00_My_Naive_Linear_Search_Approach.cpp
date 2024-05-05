ec116fbab31f120f2aaaf1471e137bd4b5da7653
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-05 19:57:53 +0530

    2154. Keep Multiplying Found Values by Two
    --> My Naive Linear Search Approach

---
+++ 01_Arrays/00_1-D Array/042_2154. Keep Multiplying Found Values by Two/00_My_Naive_Linear_Search_Approach.cpp
@@ -0,0 +1,35 @@
+// LeetCode (2154. Keep Multiplying Found Values by Two): 
+// https://leetcode.com/problems/keep-multiplying-found-values-by-two/ 
+
+
+#include<vector> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    bool isPresent(vector<int> &nums, int target){
+
+        for(int val : nums){
+            if(val == target) return true; 
+        }
+        
+        return false; 
+    }
+
+    int findFinalValue(vector<int>& nums, int original) {
+
+        while(isPresent(nums, original)){
+            original *= 2; 
+        }
+
+        return original; 
+    }
+};
+
+
+// T.C. = O((n * (n+1))/2) = O(n^2) 
+// S.C. = O(1) 
+
+// Here, n = the total number of elements which are present inside the input vector named "nums" 
