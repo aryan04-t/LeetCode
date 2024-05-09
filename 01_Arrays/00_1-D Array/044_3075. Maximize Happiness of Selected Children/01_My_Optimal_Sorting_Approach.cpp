6c7a4a405f3db96434f82cc7fe73eb25def9ea9e
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-09 11:56:49 +0530

    3075. Maximize Happiness of Selected Children
    --> My Optimal Sorting Approach

---
+++ 01_Arrays/00_1-D Array/044_3075. Maximize Happiness of Selected Children/01_My_Optimal_Sorting_Approach.cpp
@@ -0,0 +1,41 @@
+// LeetCode (3075. Maximize Happiness of Selected Children): 
+// https://leetcode.com/problems/maximize-happiness-of-selected-children/ 
+
+
+#include<vector> 
+#include<algorithm> 
+using namespace std; 
+
+
+class Solution {
+public:
+
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        
+        int n = happiness.size(); 
+        sort(happiness.begin(), happiness.end(), greater<int>()); 
+
+        long long maxHappiness = 0; 
+        int i = 0; 
+        int happinessLost = 0; 
+
+        while(k--){
+            
+            int currPersonHappiness = happiness[i] - happinessLost; 
+            
+            if(currPersonHappiness > 0) maxHappiness += currPersonHappiness; 
+            else break; 
+            
+            happinessLost++; 
+            i++;
+        }
+
+        return maxHappiness; 
+    }
+};
+
+
+// T.C. = O(n * log(n)) + O(k) = O(n * log(n)) 
+// S.C. = O(log(n)) 
+
+// Here, n = the total number of elements which are present inside the input vector named "happiness" 
