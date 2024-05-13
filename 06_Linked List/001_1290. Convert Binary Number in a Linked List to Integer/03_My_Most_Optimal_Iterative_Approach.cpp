c2042ef1c2a88639b3312cf244f401f93baffb76
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-13 19:44:10 +0530

    1290. Convert Binary Number in a Linked List to Integer
    --> My Most Optimal Iterative Approach

---
+++ 06_Linked List/001_1290. Convert Binary Number in a Linked List to Integer/03_My_Most_Optimal_Iterative_Approach.cpp
@@ -0,0 +1,50 @@
+// LeetCode (1290. Convert Binary Number in a Linked List to Integer): 
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/ 
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+// - - - - - - - - - - 
+
+
+/*
+    Definition for singly-linked list.
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+*/
+
+
+class Solution {
+public:
+
+    int getDecimalValue(ListNode* head) {
+
+        int num = 0; 
+        ListNode* temp = head; 
+
+        while(temp != nullptr){
+            num = num*2 + temp->val;  
+            temp = temp->next; 
+        } 
+
+        return num; 
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// Here, n = the total number of nodes which are present inside the input "singly linked list" 
