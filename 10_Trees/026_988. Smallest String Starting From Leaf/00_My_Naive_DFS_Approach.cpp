fb3e1465259b92c53a7d82a6910c994ddf65cd03
Aryan Tomar <aryantomar5000@gmail.com>
2024-05-17 07:01:29 +0530

    988. Smallest String Starting From Leaf
    --> My Naive DFS Approach

--- 10_Trees/026_988. Smallest String Starting From Leaf/00_My_Naive_DFS_Approach.cpp
+++ 10_Trees/026_988. Smallest String Starting From Leaf/00_My_Naive_DFS_Approach.cpp
@@ -3,6 +3,7 @@
 
 
 #include<string> 
+#include<algorithm> 
 using namespace std; 
 
 
@@ -35,47 +36,40 @@ struct TreeNode {
 class Solution {
 public:
 
-    void dfs(TreeNode* root, string &temp, string &ans){
+    void dfs(TreeNode* root, string curr, string &smallest){
 
         if(root == nullptr) return; 
 
-        temp.push_back(char(root->val + 'a')); 
+        curr.push_back(char(root->val + 'a')); 
 
         if(root->left == nullptr && root->right == nullptr){
-            
-            string revTemp = temp; 
-            reverse(revTemp.begin(), revTemp.end()); 
-
-            if(ans == ""){
-                ans = revTemp; 
-            }
+            reverse(curr.begin(), curr.end()); 
+            if(smallest == "") smallest = curr; 
             else{
-                if(revTemp < ans){
-                    ans = revTemp; 
-                }
+                if(curr < smallest) smallest = curr; 
+                return; 
             }
         }
-        
-        dfs(root->left, temp, ans); 
-        dfs(root->right, temp, ans); 
 
-        temp.pop_back(); 
+        dfs(root->left, curr, smallest); 
+        dfs(root->right, curr, smallest); 
     }
 
     string smallestFromLeaf(TreeNode* root) {
-        
-        string temp; 
-        string ans = ""; 
 
-        dfs(root, temp, ans); 
+        string curr; 
+        string smallest = "";
 
-        return ans; 
+        dfs(root, curr, smallest); 
+
+        return smallest; 
     }
 };
 
 
-// T.C. = O(((n+1)/2) - 1) * O(1)) + O(((n+1)/2) * O(m/2)) = O(n*m) = O(n*h) 
-// S.C. = O(h) + O(h) + O(h) + O(h) = (s.c. caused by the function call stack) + (s.c. caused by the "temp" string) + (s.c. caused by the "revTemp" string) + (s.c. caused by the "ans" string) = O(h) 
+// T.C. = O(((n+1)/2) - 1) * O(1)) + O(((n+1)/2) * O(m/2)) = O(n) + O(n*m) = O(n*h) 
+// S.C. = O((h * (h+1)) / 2) + O(h) = O(h^2) 
 
-// Here, n = the total number of nodes which are present inside the given binary tree, h = the height of the given binary tree 
+// Here, n = the total number of nodes which are present inside the given binary tree, 
+// h = the height of the given binary tree, 
 // and m = to the average length of all the root to leaf node strings, and in worst case m == h 
