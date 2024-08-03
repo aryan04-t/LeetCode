ce2f01256d1872d0fd1f689cda26d5c09f256163
Aryan Tomar <aryantomar5000@gmail.com>
2024-08-03 23:22:06 +0530

    2678. Number of Senior Citizens
    --> Optimal Traversal Approach

---
+++ 02_Strings/036_2678. Number of Senior Citizens/00_Optimal_Traversal_Approach.cpp
@@ -0,0 +1,25 @@
+// LeetCode (2678. Number of Senior Citizens): 
+// https://leetcode.com/problems/number-of-senior-citizens/ 
+
+
+#include<string> 
+#include<vector>  
+using namespace std; 
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0; 
+        for(const string &s : details){
+            int age = stoi(s.substr(11, 2)); 
+            if(age > 60) ans++;
+        }
+        return ans; 
+    }
+};
+
+
+// T.C. = O(n) 
+// S.C. = O(1) 
+
+// Here, n = the total number of strings which are present inside the input vector named "details" 
