68742e4bacd61049c82d93a91bf6b506af8da89a
Aryan Tomar <aryantomar5000@gmail.com>
2024-11-14 23:50:40 +0530

    2563. Count the Number of Fair Pairs
    --> My Naive Nested Loop Approach

---
+++ 05_Binary Search/009_2563. Count the Number of Fair Pairs/00_My_Naive_Nested_Loop_Approach.cpp
@@ -0,0 +1,31 @@
+// LeetCode (2563. Count the Number of Fair Pairs): 
+// https://leetcode.com/problems/count-the-number-of-fair-pairs 
+
+
+#include<vector> 
+using namespace std; 
+
+
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        
+        int n = nums.size(); 
+        int count = 0; 
+
+        for (int i=0; i < n-1; i++) {
+            for (int j=i+1; j < n; j++) {
+                int num = nums[i] + nums[j]; 
+                if (num >= lower && num <= upper) count++; 
+            }
+        }
+
+        return count; 
+    }
+};
+
+
+// T.C. = O((n*(n-1))/2) = O(n^2) 
+// S.C. = O(1) 
+
+// Here, n = total number of elements which are present inside the input vector named "nums" 
