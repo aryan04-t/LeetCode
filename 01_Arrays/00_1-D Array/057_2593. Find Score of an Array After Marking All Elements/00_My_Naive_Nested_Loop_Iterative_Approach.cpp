c3ef3121d4fb7ea3e685bfe3b990a1b0923c51f7
Aryan Tomar <aryantomar5000@gmail.com>
2024-12-13 23:57:24 +0530

    2593. Find Score of an Array After Marking All Elements
    --> My Naive Nested Loop Iterative Approach

---
+++ 01_Arrays/00_1-D Array/057_2593. Find Score of an Array After Marking All Elements/00_My_Naive_Nested_Loop_Iterative_Approach.cpp
@@ -0,0 +1,38 @@
+// LeetCode (2593. Find Score of an Array After Marking All Elements): 
+// https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements 
+
+
+#include<vector> 
+#include<algorithm>
+using namespace std; 
+
+
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        
+        long long score = 0; 
+        int n = nums.size(); 
+        vector<bool> markedElements(n, false); 
+
+        for (int i=0; i < n; i++) {
+            int mini = INT_MAX; 
+            int miniIndex = -1; 
+            for (int j=0; j < n; j++) { 
+                if (!markedElements[j] && nums[j] < mini) { 
+                    miniIndex = j; 
+                    mini = nums[j]; 
+                }
+            }
+            if (miniIndex == -1) break; 
+            else {
+                score += mini; 
+                markedElements[miniIndex] = true; 
+                if (miniIndex-1 >= 0) markedElements[miniIndex-1] = true;
+                if (miniIndex+1 < n) markedElements[miniIndex+1] = true;
+            }
+        }
+
+        return score; 
+    }
+};
